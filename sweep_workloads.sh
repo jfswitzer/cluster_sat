bad0774c0a98176cc236fc0deeded5e73773025f
Jennifer Switzer <jfi.switzer@gmail.com>
2026-04-21 07:49:31 -0700

    asdas

--- sweep_workloads.sh
+++ sweep_workloads.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+capacities=(5 15 25 35 45 55 65 75)
 datestring=$(date +"%Y-%m-%d_%H-%M-%S")
-for i in 5 15 25 35 45 55 75 85; do
-    python3 workload_w_metrics.py $1 &> $1_$datestring.out 
+for i in "${capacities[@]}"; do
+    echo $i
+    python3 workload_w_metrics.py $i &> $i_$datestring.out 
 done
